4e0c5913db98275775e553dcd45e527165dea80d
Agostino De Felice <agostino@parkingmycar.it>
2023-08-12 12:34:24 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <RouterView />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss">
+// Main Stylesheet
+@import "@/assets/scss/main";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+// All color themes are included and available by default
+// Feel free to comment out any of them if you won't use them in your project
+@import "@/assets/scss/oneui/themes/amethyst";
+@import "@/assets/scss/oneui/themes/city";
+@import "@/assets/scss/oneui/themes/flat";
+@import "@/assets/scss/oneui/themes/modern";
+@import "@/assets/scss/oneui/themes/smooth";
 </style>
